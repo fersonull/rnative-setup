243c4af4ba1c54fa4428516988b1738de8211bc8
Casper <culaspero@gmail.com>
2025-04-25 22:49:56 +0800

    added a router

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native";
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl text-blue-500 font-bold">You're all set'!</Text>
+      <Text className="text-5xl text-blue-500 font-bold">You're all set!</Text>
       <Text className="">repo by fersonull</Text>
-    </View>
+      <Link href="/onboarding" className="mt-5 text-blue-400 underline">Sample router link</Link>
+    </View >
   );
 }
